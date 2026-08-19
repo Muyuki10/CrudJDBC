c090ac9110db23cada584d78c8f2bf39a4644e13
Muyuki10 <muyuki2010@gmail.com>
2026-08-18 21:07:08 -0300

    update

--- src/main/java/connection/Conexao.java
+++ src/main/java/connection/Conexao.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Conexao {
     private static  final String URL = "jdbc:mysql://127.0.0.1:3306/Loja";
     private static final String USER = "root";
-    private static final String SENHA = "muca0310@";
+    private static final String SENHA = "" ;//usar senha propria do seu banco de dados;
 
     public static Connection conectar () {
         try{
